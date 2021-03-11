c98acb5c9628c2ec370135e6dcbf6cfa51701e9e
ColinBloemendaal-COAR <colin.bloemendaal@gmail.com>
2021-03-11 13:54:13 +0100

    Addes /msg tab completion

---
+++ src/me/COAR/CoarMcServer/Essentials/Msg/MsgTabCompletion.java
@@ -0,0 +1,44 @@
+package me.COAR.CoarMcServer.Essentials.Msg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import me.COAR.CoarMcServer.Main;
+
+public class MsgTabCompletion implements TabCompleter {
+	private Main main;
+	public MsgTabCompletion(Main plugin) {
+		this.main = plugin;
+	}
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command c, String lbl, String[] args) {
+		Player player = (Player) sender;
+		List<String> tabArguments = new ArrayList<String>();
+		List<String> tabResults = new ArrayList<String>();
+		if(args.length == 1) {
+			for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+				if(p.getName().equals(player.getDisplayName()))
+					continue;
+				if(p.hasPermission("CoarMcServer.Msg.Others.Unable"))
+					continue;
+				tabArguments.add(p.getDisplayName());
+			}
+			for(String tA : tabArguments) {
+				if(tA.toLowerCase().startsWith(args[0].toLowerCase())) {
+					tabResults.add(tA);
+				}
+			}
+			return tabResults;
+		}	else if(args.length >= 2) {
+			return tabResults;
+		}
+		return tabResults;
+	}
+
+}
