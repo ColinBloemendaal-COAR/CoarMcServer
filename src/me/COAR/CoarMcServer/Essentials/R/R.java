897228f1804956157a33da6da7f4beb881b6ef80
ColinBloemendaal-COAR <colin.bloemendaal@gmail.com>
2021-03-11 15:39:48 +0100

    naar laptop

--- src/me/COAR/CoarMcServer/Essentials/R/R.java
+++ src/me/COAR/CoarMcServer/Essentials/R/R.java
@@ -1,5 +1,33 @@
 package me.COAR.CoarMcServer.Essentials.R;
 
-public class R {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
+import me.COAR.CoarMcServer.Main;
+
+public class R implements CommandExecutor{
+	private Main main;
+	public R(Main plugin) {
+		this.main = plugin;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command c, String lbl, String[] args) {
+		if(lbl.equalsIgnoreCase("r")) {
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+				Player p = Bukkit.getServer().getPlayer(main.seplayer.getPlayerDataString(player, player.getUniqueId().toString(), "PreviousMessage"));
+			} else if(sender instanceof ConsoleCommandSender) {
+				
+				sender.sendMessage(main.functions.TCC(main.messages.Get("")));
+				return false;
+			}
+		}
+		
+		
+		return false;
+	}
 }
